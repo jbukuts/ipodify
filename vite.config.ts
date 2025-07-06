89f29014dbed142c3c376b66229ff86d1e562d4b
jbukuts <jbukuts@gmail.com>
2025-07-06 16:55:22 -0400

    feat: add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/ipodify',
   resolve: {
     alias: {
       '#': resolve(__dirname, 'src')
