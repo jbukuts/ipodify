2ba819b1602bf1c85422abd81dbbfc10d29edfc9
jbukuts <jbukuts@gmail.com>
2025-07-11 17:50:52 -0400

    feat: queue screen can play songs

--- src/components/windows/queue.tsx
+++ src/components/windows/queue.tsx
@@ -1,25 +1,62 @@
 import { sdk } from '#/lib/sdk';
 import { useQuery } from '@tanstack/react-query';
 import Screen from '../shared/screen';
-import TrackItem from '../shared/track-item';
 import { QUERY_KEYS } from '#/lib/query-enum';
+import usePlaySong from '#/hooks/usePlaySong';
+import { useCallback, useMemo } from 'react';
+import usePlaybackStateStore from '#/lib/store/playback-state-store';
+import { useShallow } from 'zustand/react/shallow';
+import type { TrackItem as TrackItemType } from '@spotify/web-api-ts-sdk';
+import TrackItem from '../shared/track-item';
 
 export default function Queue() {
+  const playSong = usePlaySong();
+  const contextUri = usePlaybackStateStore(useShallow((s) => s.context?.uri));
+
   const { data, isFetching } = useQuery({
     queryKey: [QUERY_KEYS.player.QUEUE],
     initialData: { queue: [], currently_playing: null },
     queryFn: () => sdk.player.getUsersQueue()
   });
 
-  const allRows = [
-    ...(data.currently_playing ? [data.currently_playing] : []),
-    ...data.queue
-  ];
+  const queueTracks = useMemo(
+    () => [
+      ...(data.currently_playing ? [data.currently_playing] : []),
+      ...data.queue
+    ],
+    [data]
+  );
+
+  const handleClick = useCallback(
+    (track: TrackItemType, idx: number) => {
+      if (contextUri)
+        return playSong({
+          contextUri,
+          offset: track.uri
+        });
+
+      const range = queueTracks
+        .slice(Math.max(idx - 5, 0), Math.min(idx + 5, queueTracks.length - 1))
+        .map((t) => t.uri);
+      const offset = range.findIndex((s) => track.uri === s);
+
+      playSong({
+        uris: range,
+        offset
+      });
+    },
+    [playSong, contextUri, queueTracks]
+  );
 
   return (
     <Screen loading={isFetching && data.currently_playing === null}>
-      {allRows.map((track, idx) => (
-        <TrackItem key={idx} trackNumber={idx + 1} track={track} />
+      {queueTracks.map((track, idx) => (
+        <TrackItem
+          key={idx}
+          trackNumber={idx + 1}
+          track={track}
+          onClick={() => handleClick(track, idx)}
+        />
       ))}
     </Screen>
   );
